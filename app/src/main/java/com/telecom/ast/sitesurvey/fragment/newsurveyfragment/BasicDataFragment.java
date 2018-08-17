6ce88e4dfc59fc6597041831a4c9325e2e3bd533
neeraj <neerajk0702@gmail.com>
2018-08-17 12:15:39 +0530

    add location

--- app/src/main/java/com/telecom/ast/sitesurvey/fragment/newsurveyfragment/BasicDataFragment.java
+++ app/src/main/java/com/telecom/ast/sitesurvey/fragment/newsurveyfragment/BasicDataFragment.java
@@ -1,8 +1,17 @@
 package com.telecom.ast.sitesurvey.fragment.newsurveyfragment;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.AppCompatEditText;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +20,20 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.telecom.ast.sitesurvey.R;
 import com.telecom.ast.sitesurvey.component.ASTProgressBar;
@@ -31,7 +54,9 @@ import com.telecom.ast.sitesurvey.utils.ASTUIUtil;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import okhttp3.MediaType;
@@ -68,6 +93,14 @@ public class BasicDataFragment extends MainFragment {
     private String[] arrCustomerSiteId;
     private long dateTimeMili;
 
+    private String currentLongitude = "";
+    private String currentLatitude = "";
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private long UPDATE_INTERVAL = 10 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+
     @Override
     protected int fragmentLayout() {
         return R.layout.activity_basic_data;
@@ -92,6 +125,8 @@ public class BasicDataFragment extends MainFragment {
         etCaretaker = findViewById(R.id.etCaretaker);
         etCaretakercontact = findViewById(R.id.etCaretakercontact);
         etownerAddress = findViewById(R.id.etownerAddress);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
+        //startLocationUpdates();
     }
 
     @Override
@@ -110,6 +145,7 @@ public class BasicDataFragment extends MainFragment {
         getUserPref();
         getSaveBasicDataSharedPref();
         getBasicData();
+
     }
 
 
@@ -494,8 +530,8 @@ public class BasicDataFragment extends MainFragment {
                 BasicData.put("OwnerAddress", ownerAddress);
                 BasicData.put("CareTaker", Caretaker);
                 BasicData.put("CareTakerNo", Caretakercontact);
-                BasicData.put("Latitude", "23.45678");
-                BasicData.put("Longitude", "23.45678");
+                BasicData.put("Latitude", currentLatitude);
+                BasicData.put("Longitude", currentLongitude);
 
                 jsonObject.put("BasicData", BasicData);
 
@@ -557,4 +593,129 @@ public class BasicDataFragment extends MainFragment {
         return multipartBody;
     }
 
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+
+        // Create the location request to start receiving updates
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        // Create LocationSettingsRequest object using location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+
+        // Check whether location settings are satisfied
+        // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+        SettingsClient settingsClient = LocationServices.getSettingsClient(getContext());
+        settingsClient.checkLocationSettings(locationSettingsRequest);
+        Task<LocationSettingsResponse> task = settingsClient.checkLocationSettings(builder.build());
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+               /* for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                }*/
+                onLocationChanged(locationResult.getLastLocation());
+            }
+        };
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+       /* mFusedLocationClient.requestLocationUpdates(mLocationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        if (locationResult == null) {
+                            return;
+                        }
+                        onLocationChanged(locationResult.getLastLocation());
+                    }
+                },
+                Looper.myLooper());*/
+    }
+
+    //get location change
+    public void onLocationChanged(Location location) {
+        // New location has now been determined
+        currentLongitude = Double.toString(location.getLongitude());
+        currentLatitude = Double.toString(location.getLatitude());
+
+        String msg = "Updated Location: " +
+                Double.toString(location.getLatitude()) + "," +
+                Double.toString(location.getLongitude());
+        Log.d(Contants.LOG_TAG, "Current Location*******" + msg);
+
+      /*  Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+        // You can now create a LatLng Object for use with maps
+         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());*/
+    }
+
+    //get last location
+    public void getLastLocation() {
+        // Get last known recent location using new Google Play Services SDK (v11+)
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                // GPS location can be null if GPS is switched off
+                if (location != null) {
+                    onLocationChanged(location);
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(Contants.LOG_TAG, "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mFusedLocationClient != null) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    //stop location update
+    private void stopLocationUpdates() {
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
 }
